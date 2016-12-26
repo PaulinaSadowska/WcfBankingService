07cc1e04114bb9c0ac527342b2d5aaf7c3a15a29
Paulina Sadowska <paulina.sadowska@student.put.poznan.pl>
2016-12-26 14:57:03 +0100

    move bank logic to Bank class

---
+++ WcfBankingService/Bank.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using WcfBankingService.Accounts.Number;
+using WcfBankingService.Accounts.Number.ControlSum;
+using WcfBankingService.operation;
+using WcfBankingService.operation.operations;
+using WcfBankingService.Operation.Operations;
+using WcfBankingService.SOAPService.DataContract;
+using WcfBankingService.Users;
+
+namespace WcfBankingService
+{
+    public class Bank
+    {
+        private const string BankId = "11216900";
+
+        private readonly UserManager _userManager;
+        private readonly AccountNumberFactory _accountNumberFactory;
+
+        public Bank()
+        {
+            _userManager = new UserManager();
+            _accountNumberFactory = new AccountNumberFactory(BankId, new StandardControlSumCalculator());
+        }
+
+        public string SignIn(string login, string password)
+        {
+            return _userManager.SignIn(login, password);
+        }
+
+        public void Deposit(PaymentData paymentData)
+        {
+            var account = _userManager.GetAccount(paymentData.AccessToken, _accountNumberFactory.GetAccountNumber(paymentData.AccountNumber));
+            new Deposit(account, paymentData.Amount, paymentData.OperationTitle).Execute();
+        }
+
+        public void Withdraw(PaymentData paymentData)
+        {
+            var account = _userManager.GetAccount(paymentData.AccessToken, _accountNumberFactory.GetAccountNumber(paymentData.AccountNumber));
+            new Withdraw(account, paymentData.Amount, paymentData.OperationTitle).Execute();
+        }
+
+        public IEnumerable<OperationRecord> GetOperationHistory(string accessToken, string accountNumber)
+        {
+            var account = _userManager.GetAccount(accessToken, _accountNumberFactory.GetAccountNumber(accountNumber));
+            return account.GetOperationHistory();
+        }
+    }
+}
--- WcfBankingService/SoapService/BankingService.svc.cs
+++ WcfBankingService/SoapService/BankingService.svc.cs
@@ -1,41 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
-using WcfBankingService.Accounts.Number;
-using WcfBankingService.Accounts.Number.ControlSum;
 using WcfBankingService.operation;
-using WcfBankingService.operation.operations;
-using WcfBankingService.Operation.Operations;
 using WcfBankingService.SoapService.DataContract.Response;
 using WcfBankingService.SoapService.Validation;
 using WcfBankingService.SOAPService.DataContract;
-using WcfBankingService.Users;
 
 namespace WcfBankingService.SoapService
 {
     public class BankingService : IBankingService
     {
         private readonly IServiceInputValidator _inputValidator;
-        private readonly UserManager _userManager;
-        private readonly AccountNumberFactory _accountNumberFactory;
+        private readonly Bank _bank;
+
 
         public BankingService()
         {
             _inputValidator = new ServiceInputValidator();
-            _userManager = new UserManager();
-            _accountNumberFactory = new AccountNumberFactory("11216900", new StandardControlSumCalculator());
+            _bank = new Bank();
         }
 
         public LogInResponse SignIn(string login, string password)
         {
             _inputValidator.CheckSignInDataValid(login, password);
-            return new LogInResponse(_userManager.SignIn(login, password));
+            return new LogInResponse(_bank.SignIn(login, password));
         }
 
         public PaymentResponse Deposit(PaymentData paymentData)
         {
             _inputValidator.CheckPaymentData(paymentData);
-            var account = _userManager.GetAccount(paymentData.AccessToken, _accountNumberFactory.GetAccountNumber(paymentData.AccountNumber));
-            new Deposit(account, paymentData.Amount, paymentData.OperationTitle).Execute();
+            _bank.Deposit(paymentData); // TODO - add return type (operation status)
             return new PaymentResponse();
         }
 
@@ -47,16 +40,14 @@ namespace WcfBankingService.SoapService
         public PaymentResponse Withdraw(PaymentData paymentData)
         {
             _inputValidator.CheckPaymentData(paymentData);
-            var account = _userManager.GetAccount(paymentData.AccessToken, _accountNumberFactory.GetAccountNumber(paymentData.AccountNumber));
-            new Withdraw(account, paymentData.Amount, paymentData.OperationTitle).Execute();
+            _bank.Withdraw(paymentData);
             return new PaymentResponse();
         }
 
         public IEnumerable<OperationRecord> GetOperationHistory(string accessToken, string accountNumber)
         {
-           //TODO - validate account number
-            var account = _userManager.GetAccount(accessToken, _accountNumberFactory.GetAccountNumber(accountNumber));
-            return account.GetOperationHistory();
+           //TODO - validate input data
+            return _bank.GetOperationHistory(accessToken, accountNumber);
             //TODO - change type to OperationHistoryResponse to send error codes
         }
     }
