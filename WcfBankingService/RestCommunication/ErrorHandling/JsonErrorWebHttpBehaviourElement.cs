e1788213576fdc661f98f8b1c842010839524673
Paulina Sadowska <paulina.sadowska@student.put.poznan.pl>
2017-01-19 16:58:33 +0100

    Authorization error message format

---
+++ WcfBankingService/RestCommunication/ErrorHandling/JsonErrorWebHttpBehaviourElement.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ServiceModel.Configuration;
+
+namespace WcfBankingService.RestCommunication.ErrorHandling
+{
+    public class JsonErrorWebHttpBehaviorElement : BehaviorExtensionElement
+    {
+        /// 
+        /// Get the type of behavior to attach to the endpoint
+        /// 
+        public override Type BehaviorType => typeof(JsonErrorWebHttpBehavior);
+
+        /// 
+        /// Create the custom behavior
+        /// 
+        protected override object CreateBehavior()
+        {
+            return new JsonErrorWebHttpBehavior();
+        }
+    }
+}
