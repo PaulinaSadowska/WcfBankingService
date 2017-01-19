67e66ba5ce617abbbcdbb8bb3bc44effe3610743
Paulina Sadowska <paulina.sadowska@student.put.poznan.pl>
2017-01-19 18:59:56 +0100

    IAccount and IPublicAccount comments

--- WcfBankingService/Accounts/IAccount.cs
+++ WcfBankingService/Accounts/IAccount.cs
@@ -3,9 +3,21 @@ using WcfBankingService.operation;
 
 namespace WcfBankingService.Accounts
 {
+    /// <summary>
+    /// Interface used to perform actions on the account (including actions which require authorization)
+    /// </summary>
     public interface IAccount : IPublicAccount
     {
+        /// <summary>
+        /// Substract amount from account balance value 
+        /// </summary>
+        /// <param name="amount">amount to substract</param>
         void SubstractFromBalance(decimal amount);
+
+        /// <summary>
+        /// Get history of all operations
+        /// </summary>
+        /// <returns>list of all operations performed on this account</returns>
         IEnumerable<OperationRecord> GetOperationHistory();
     }
 }
--- WcfBankingService/Accounts/IPublicAccount.cs
+++ WcfBankingService/Accounts/IPublicAccount.cs
@@ -3,11 +3,29 @@ using WcfBankingService.operation;
 
 namespace WcfBankingService.Accounts
 {
+    /// <summary>
+    /// Interface used to perform actions on the account which don't require authorization
+    /// </summary>
     public interface IPublicAccount
     {
-            AccountNumber AccountNumber { get; }
-            void AddToBalance(decimal amount);
-            void AddOperationToHistory(OperationRecord operationRecord);
-            decimal GetBalanceValue();
-        }
+        AccountNumber AccountNumber { get; }
+
+        /// <summary>
+        /// Add amount to balance value
+        /// </summary>
+        /// <param name="amount">amount to add</param>
+        void AddToBalance(decimal amount);
+
+        /// <summary>
+        /// Add operation record to account operation history
+        /// </summary>
+        /// <param name="operationRecord">operation record</param>
+        void AddOperationToHistory(OperationRecord operationRecord);
+
+        /// <summary>
+        /// Get current balance value
+        /// </summary>
+        /// <returns>balance value</returns>
+        decimal GetBalanceValue();
+    }
 }
